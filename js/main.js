3f2d5b01d6871ab43ec5299ec2fe42031e06d55d
Rimich666 <89179699365@mail.ru>
2022-10-14 18:04:30 +0400

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let randomInt = function (start, end){
+  let max = Math.floor(Math.max(start, end));
+  let min = Math.ceil(Math.min(start, end));
+  if (min<0){
+    console.error(`Границы интервала должны быть неотрицательными`);
+    return NaN;
+  }
+  return Math.round((max-min) * Math.random()) + min;
+};
+
+let floorFraction = (val, precision)=>
+  Math.floor(val * (10 ** precision)) / (10 ** precision);
+
+let ceilFraction = (val, precision)=>
+  Math.ceil(val * (10 ** precision)) / (10 ** precision);
+
+let roundFraction = (val, precision)=>
+  Math.round(val * (10 ** precision)) / (10 ** precision);
+
+let randomFloat = function(start, end, precision){
+  if (!Number.isInteger(precision)){
+    console.warn(`Количество знаков: ${precision} - дробное, буду округлять`);
+    precision = Math.round(precision);
+  }
+  if (precision<0){
+    console.warn(`Количество знаков: ${precision} - отрицательное, буду обнулять`);
+    precision = 0;
+  }
+  let max = floorFraction(Math.max(start, end), precision);
+  let min = ceilFraction(Math.min(start, end), precision);
+  if (min<0){
+    console.error(`Границы интервала должны быть неотрицательными, вот так`);
+    return NaN;
+  }
+  return roundFraction((max-min) * Math.random() + min, precision);
+  //return roundFraction((max-min) * Math.random(), precision) + min;
+};
+
+console.log(randomFloat(10, 1, 3));
+console.log(randomInt(1, 2));
