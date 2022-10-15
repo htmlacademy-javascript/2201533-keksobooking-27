2a839922642363a2ac97f23359d1154487a68378
Rimich666 <89179699365@mail.ru>
2022-10-15 21:09:15 +0400

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,11 +1,59 @@
-const randomInt = function (start, end){
+const DATA_SIZE = 10;
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const sentences = [
+  'Великолепная квартира-студия в центре Токио.',
+  'Подходит как туристам, так и бизнесменам.',
+  'Квартира полностью укомплектована и недавно отремонтирована.',
+  'Ремонт делали год назад, все свежее и уютное.',
+  'В коридоре в районе крючков для курток вздулись обои, но под верхней одеждой эти дефекты не видны.',
+  'В гостиной есть диван, стеллаж, подставка под телевизор.',
+  'В обеих спальнях есть двуспальные кровати с только что купленными матрасами, шкаф-купе, зеркало.',
+  'Высокоскоростной интернет.',
+  'Полностью оборудованная кухня: холодильник, плита с 4 конфорками, духовка, микроволновка, электрический чайник, небольшой набор посуды.',
+  'Санузел совмещенный, есть стиральная машина.',
+  'Из окна виден парк.'
+];
+const photos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checks = ['12:00', '13:00', '14:00'];
+const BORDERS = {
+  price: {
+    min: 3000,
+    max: 300000
+  },
+  rooms: {
+    min: 1,
+    max: 14
+  },
+  guests: {
+    min: 1,
+    max: 34
+  },
+  location: {
+    lat: {
+      min: 35.65000,
+      max: 35.70000
+    },
+    lng: {
+      min: 139.70000,
+      max: 139.80000
+    }
+  }
+};
+const COORDINATES_PRECISION = 5;
+const PHOTO_COUNT = 10;
+
+const getRandomInt = (start, end) =>{
   const max = Math.floor(Math.max(start, end));
   const min = Math.ceil(Math.min(start, end));
   if (min < 0){
-    //console.error(`Границы интервала должны быть неотрицательными`);
     return NaN;
   }
-  return Math.round((max - min) * Math.random()) + min;
+  return Math.floor((max - min + 1) * Math.random() + min);
 };
 
 const floorFraction = (val, precision)=>
@@ -17,147 +65,96 @@ const ceilFraction = (val, precision)=>
 const roundFraction = (val, precision)=>
   Math.round(val * (10 ** precision)) / (10 ** precision);
 
-const randomFloat = function(start, end, precision){
+const getRandomFloat = (start, end, precision)=>{
   if (!Number.isInteger(precision)){
-    //console.warn(`Количество знаков: ${precision} - дробное, буду округлять`);
     precision = Math.round(precision);
   }
   if (precision < 0){
-    //console.warn(`Количество знаков: ${precision} - отрицательное, буду обнулять`);
     precision = 0;
   }
   const max = floorFraction(Math.max(start, end), precision);
   const min = ceilFraction(Math.min(start, end), precision);
   if (min < 0){
-    //console.error(`Границы интервала должны быть неотрицательными, вот так`);
     return NaN;
   }
   return roundFraction((max - min) * Math.random() + min, precision);
 };
 
-/*console.log(randomFloat(10, 1, 3));
-console.log(randomInt(1, 2));*/
-
-/*randomFloat(10, 1, 3);
-randomInt(1, 2);*/
-
-const getLocation = function(){
+const getLocation = ()=>{
   return {
-    lat: randomFloat(35.65000, 35.70000, 5),
-    lng: randomFloat(139.70000, 139.80000, 5)
+    lat: getRandomFloat(BORDERS.location.lat.min, BORDERS.location.lat.max, COORDINATES_PRECISION),
+    lng: getRandomFloat(BORDERS.location.lng.min, BORDERS.location.lng.max, COORDINATES_PRECISION)
   };
 };
 
-const getTitle = function(){
-  const res = [];
-  const words = [
-    ['Уютное', 'гнездышко', 'молодоженов'],
-    ['Убогое', 'лежбище', 'наркоманов'],
-    ['Помпезное', 'жилище', 'нарциссов'],
-    ['Превосходное', 'помещение', 'бездомных'],
-    ['Прекрасное', 'место', 'туристов'],
-    ['Скрытное', 'пристанище', 'злых гангстеров']
-  ];
-  for (let i = 0; i < 3; i++){
-    const j = randomInt(0, 5);
-    res[i] = words[j][i];
+let Title = {
+  words :[
+    ['Уютное', 'гнездышко', 'для молодоженов'],
+    ['Убогое', 'лежбище', 'для наркоманов'],
+    ['Помпезное', 'жилище', 'для нарциссов'],
+    ['Превосходное', 'помещение', 'для бездомных'],
+    ['Прекрасное', 'место', 'для туристов'],
+    ['Скрытное', 'пристанище', 'для злых гангстеров']
+  ],
+
+  get: function(){
+    const res = [];
+    for (let i = 0; i < this.words[0].length; i++){
+      const j = getRandomInt(0, this.words.length - 1);
+      res[i] = (this.words)[j][i];
+    }
+    return res.join(' ');
   }
-  res[3] = res[2];
-  res[2] = 'для';
-  return res.join(' ');
 };
 
-const kFromN = function(variants){
-  const select = [];
-  const size = randomInt(1, variants.length);
+const getUniqRandomArray = (variants)=>{
+  const chosen = [];
+  const size = getRandomInt(1, variants.length);
   for (let i = 0; i < size; i++){
-    const j = randomInt(0, variants.length - 1);
-    select[i] = variants[j];
+    const j = getRandomInt(0, variants.length - 1);
+    chosen[i] = variants[j];
     variants.splice(j, 1);
   }
-  return select;
+  return chosen;
 };
 
-const getFeatures = function(){
-  const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  return kFromN(features.slice());
+const getFeatures = ()=>getUniqRandomArray(FEATURES.slice());
+
+const getDescription = ()=>getUniqRandomArray(sentences.slice()).join(' ');
+
+const getPhotos = () =>{
+  const arrayPhotos = Array(getRandomInt(1, PHOTO_COUNT)).fill(0);
+  return arrayPhotos.map(e=>photos[getRandomInt(0, photos.length - 1)]);
 };
 
-const getDescription = function(){
-  const sentences = [
-    'Великолепная квартира-студия в центре Токио.',
-    'Подходит как туристам, так и бизнесменам.',
-    'Квартира полностью укомплектована и недавно отремонтирована.',
-    'Ремонт делали год назад, все свежее и уютное.',
-    'В коридоре в районе крючков для курток вздулись обои, но под верхней одеждой эти дефекты не видны.',
-    'В гостиной есть диван, стеллаж, подставка под телевизор.',
-    'В обеих спальнях есть двуспальные кровати с только что купленными матрасами, шкаф-купе, зеркало.',
-    'Высокоскоростной интернет.',
-    'Полностью оборудованная кухня: холодильник, плита с 4 конфорками, духовка, микроволновка, электрический чайник, небольшой набор посуды.',
-    'Санузел совмещенный, есть стиральная машина.',
-    'Из окна виден парк.'
-  ];
-  return kFromN(sentences.slice()).join(' ');
-};
+const getAvatar = (num) =>`img/avatars/user${num.toString().padStart(2,'0')}.png`;
 
-const getPhotos = function(){
-  const maxSize = 10;
-  const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-  ];
-  const select = [];
-  const size = randomInt(1, maxSize);
-  for ( let i = 0; i < size; i++){
-    select[i] = photos[randomInt(0, photos.length - 1)];
-  }
-  return select;
-};
+const getRandomElement = (array) =>array[getRandomInt(0, array.length - 1)]
 
-const getAvatar = function(num){
-  let numStr = num.toString();
-  const mask = '00';
-  numStr = mask.substr(0, mask.length - numStr.length) + numStr;
-  return `img/avatars/user${numStr}.png`;
-};
-
-const createItem = function(num){
-  const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  const checks = ['12:00', '13:00', '14:00'];
+const createItem = (num)=>{
   const location = getLocation();
   return {
     author: {
       avatar: getAvatar(num + 1)
     },
     offer: {
-      title: getTitle(),
+      title: Title.get(),
       address: `${location.lat}, ${location.lng}`,
-      price: randomInt(3000, 300000),
-      type: types[randomInt(0, 4)],
-      rooms: randomInt(1, 14),
-      guests: randomInt(1, 34),
-      checkin: checks[randomInt(0, 2)],
-      checkout: checks[randomInt(0, 2)],
+      price: getRandomInt(BORDERS.price.min, BORDERS.price.max),
+      type: getRandomElement(types),
+      rooms: getRandomInt(BORDERS.rooms.min, BORDERS.rooms.max),
+      guests: getRandomInt(BORDERS.guests.min, BORDERS.guests.max),
+      checkin: getRandomElement(checks),
+      checkout: getRandomElement(checks),
       features: getFeatures(),
       description: getDescription(),
       photos: getPhotos(),
     },
-    location: {
-      lat: location.lat,
-      lng: location.lng
-    }
+    location: location
   };
 };
 
-
-const createData = function(){
-  const DATASIZE = 10;
-  const data = [];
-  for(let i = 0; i < DATASIZE; i++){
-    data[i] = createItem(i);
-  }
-  return data;
-};
+const createData = ()=>Array(DATA_SIZE).fill(0).map((e, i)=>createItem(i));
 
 //console.log(createData());
 createData();
